af6974aa113ea3fb3be4d3b2758a11d43a6bff75
jiebaoking <31099707+jiebaoking@users.noreply.github.com>
2018-02-27 14:26:28 +0800

    为确保跟单可以跟进，当前买卖单均调整了2%，买价挂高2%，卖价挂低2%

--- easytrader/clienttrader.py
+++ easytrader/clienttrader.py
@@ -186,12 +186,12 @@ class ClientTrader:
 
     def buy(self, security, price, amount, **kwargs):
         self._switch_left_menus(['买入[F1]'])
-
+        price=round(price*1.02)
         return self.trade(security, price, amount)
 
     def sell(self, security, price, amount, **kwargs):
         self._switch_left_menus(['卖出[F2]'])
-
+        price=round(price*0.98)
         return self.trade(security, price, amount)
 
     def market_buy(self, security, amount, ttype=None, **kwargs):
